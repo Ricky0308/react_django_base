374e4831bf11d4f639209894f31570639049968f
Ricky0308 <green0308@softbank.ne.jp>
2025-01-30 15:21:28 +0900

    prevent login token exposure

--- backend/Profile/views.py
+++ backend/Profile/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.middleware.csrf import get_token
 
 
 
 class HelloWorld(APIView):
     def get(self, request):
-        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
+        print(request)
+        return Response({"message": "Hello, world!"})
+
+    def post(self, request):
+        
+        cookie_token = request.COOKIES.get('csrftoken', 'No csrftoken cookie')
+        header_token = request.META.get('HTTP_X_CSRFTOKEN', 'No X-CSRFToken header')
+        print("get_token:", get_token(request))
+        print("cookie_token:", cookie_token)
+        print("header_token:", header_token)
+        return Response({"message": "Hello, world!"})
