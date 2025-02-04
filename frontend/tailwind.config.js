52b43500f87c592beb2a2127af207431eed08257
Ricky0308 <green0308@softbank.ne.jp>
2025-02-04 21:54:18 +0900

    modify tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightblue: {
+          500: '#ADD8E6', // Light blue color
+          200: '#B0E0E6', // Lighter shade for hover
+        },
+      },
+    },
   },
   plugins: [],
 }
