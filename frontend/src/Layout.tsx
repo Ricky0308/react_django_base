b0de82401d89d7451fe34ef8d79ee1d39424b157
Ricky0308 <green0308@softbank.ne.jp>
2025-02-16 19:54:55 +0900

    prevent auto centering in Layout

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="flex-grow container mx-auto p-4">
+      <main className="flex-grow px-0">
         <Outlet />
       </main>
       <footer className="bg-lightblue-500 text-white p-4 text-center">
