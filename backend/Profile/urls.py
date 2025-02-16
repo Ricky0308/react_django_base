2f750de7dee187c1e06fba6e917721e646614612
Ricky0308 <green0308@softbank.ne.jp>
2025-02-16 19:07:23 +0900

    add profile update api endpoint

--- backend/Profile/urls.py
+++ backend/Profile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HelloWorld
+from .views import HelloWorld, ProfileUpdateView
 
 urlpatterns = [
     path('hello/', HelloWorld.as_view(), name='hello-world'),
+    path('update/', ProfileUpdateView.as_view(), name='profile-update'),
 ]
