aa43c9c774d219a63acb68078a4a602679ba1987
Ricky0308 <green0308@softbank.ne.jp>
2025-02-16 18:47:57 +0900

    add email pending and pending expiration to user model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,6 +1,8 @@
 import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 class User(AbstractUser):
     """
@@ -11,3 +13,10 @@ class User(AbstractUser):
         default=uuid.uuid4,
         editable=False
     )
+    pending_email = models.EmailField(blank=True, null=True)
+    pending_email_expiration = models.DateTimeField(blank=True, null=True)
+
+    def set_pending_email(self, email):
+        self.pending_email = email
+        self.pending_email_expiration = timezone.now() + timedelta(hours=1)  # 1 hour expiration
+        self.save()
